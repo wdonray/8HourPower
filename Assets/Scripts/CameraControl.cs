dcbe98e37720d9bcc13e9614affa66acaf5453fb
Donray Williams <w.donray@yahoo.com>
2017-05-05 10:03:38 -0500

    Started

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    public GameObject The_Player;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - The_Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = The_Player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text Pos_Text;
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Pos_Text.text = rb.transform.position.ToString();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Pos_Text.text = rb.transform.position.ToString();
+    }
+    void FixedUpdate()
+    {
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHor, 0.0f, moveVert);
+
+        rb.AddForce(movement * speed);
+    }
+}
